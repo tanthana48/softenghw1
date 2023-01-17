4062ceb6ff2e493c42674d5cdda193fb68c8f52d
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-01-17 15:57:44 +0700

    connect to database

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
-
+from flask import Flask, request
+from flask_mysqldb import MySQL
+import yaml
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+cred = yaml.load(open('cred.yaml'), Loader=yaml.Loader)
+app.config['MYSQL_HOST'] = cred['mysql_host']
+app.config['MYSQL_USER'] = cred['mysql_user']
+app.config['MYSQL_PASSWORD'] = cred['mysql_password']
+app.config['MYSQL_DB'] = cred['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
+def create_table():
+    cursor = mysql.connection.cursor()
+    mysql.connection.commit()
+    cursor.close()
+    return
+@app.route("/list")
+def list():
     
 	return
 
