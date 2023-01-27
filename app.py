da83e4706001c96a506ec45d7fbb9d7d0c395024
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-01-27 13:20:57 +0700

    fix snake_case and table

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 import yaml
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Never push this line to github public repo"
 
@@ -12,12 +13,13 @@ app.config['MYSQL_DB'] = cred['mysql_db']
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
+
 @app.route("/list")
 def list():
     cur = mysql.connection.cursor()
-    queryStatement = f"SELECT * FROM vendingmachine"
-    print(queryStatement)
-    result_value = cur.execute(queryStatement) 
+    query_statement = f"SELECT * FROM vendingmachine"
+    print(query_statement)
+    result_value = cur.execute(query_statement)
     if result_value > 0:
         machines = jsonify(cur.fetchall())
     else:
@@ -26,32 +28,35 @@ def list():
     cur.close()
     return machines
 
+
 @app.route("/create-machine", methods=['POST'])
-def createMachine():
-    if request.method == 'POST':
-        name = request.args.get("name")
-        location = request.args.get("location")
-        print(name)
-        print(location)
-        if name == None or location == None:
-            return "Plese put both name and location"
-        cur = mysql.connection.cursor()
-        queryStatement = f"INSERT INTO vendingmachine(machine_name, location, product) values ('{name}', '{location}', json_object())"
-        print(queryStatement)
-        cur.execute(queryStatement) 
-        mysql.connection.commit()
-        cur.close()
-        return "Sucessfully Created"
-    else:
-        return None
+def create_machine():
+    try:
+        if request.method == 'POST':
+            name = request.args.get("name")
+            location = request.args.get("location")
+            print(name)
+            print(location)
+            if name == None or location == None:
+                return "Please put both name and location"
+            cur = mysql.connection.cursor()
+            query_statement = f"INSERT INTO vendingmachine(machine_name, location) values ('{name}', '{location}')"
+            print(query_statement)
+            cur.execute(query_statement)
+            mysql.connection.commit()
+            cur.close()
+            return jsonify(message='Successfully Created')
+    except Exception as e:
+        return jsonify(error=str(e))
+
 
 @app.route('/delete-machine/<int:id>/', methods=['GET'])
-def deleteMachine(id):
+def delete_machine(id):
     if request.method == 'GET':
         cur = mysql.connection.cursor()
-        queryStatement = f"DELETE FROM vendingmachine WHERE id = {id}"
-        print(queryStatement)
-        result = cur.execute(queryStatement)
+        query_statement = f"DELETE FROM vendingmachine WHERE id = {id}"
+        print(query_statement)
+        result = cur.execute(query_statement)
         if result == 0:
             return "There is no this machine"
         mysql.connection.commit()
@@ -59,31 +64,32 @@ def deleteMachine(id):
         return "Successfully Deleted"
     else:
         return None
-    
+
+
 @app.route("/add-product", methods=['POST'])
-def addProduct():
+def add_product():
     if request.method == 'POST':
         cur = mysql.connection.cursor()
         id = request.args.get("machineid")
-        productName = request.args.get("name")
+        product_name = request.args.get("name")
         amount = request.args.get("amount")
-        if productName == None or amount == None or id == None:
+        if product_name == None or amount == None or id == None:
             return "Plese put machineid, product and amount"
-        checkIdStatement = f"Select id from vendingmachine where id = {id}"
-        result_value = cur.execute(checkIdStatement)
-        #check whether there is this machine id or not 
+        check_id_statement = f"Select id from vendingmachine where id = {id}"
+        result_value = cur.execute(check_id_statement)
+        # check whether there is this machine id or not
         if result_value > 0:
-            checkProductStatement = f"Select json_extract(product, '$.{productName}') from vendingmachine where id = {id} and JSON_EXTRACT(product, '$.{productName}') IS NOT NULL"
-            productresult = cur.execute(checkProductStatement)
-            #checkwhether there is this product or not
-            if productresult > 0:
+            check_product_statement = f"Select json_extract(product, '$.{product_name}') from vendingmachine where id = {id} and JSON_EXTRACT(product, '$.{product_name}') IS NOT NULL"
+            product_result = cur.execute(check_product_statement)
+            # check whether there is this product or not
+            if product_result > 0:
                 return "This product is already on the machine"
             else:
-                addStatement = f"update vendingmachine set product = json_insert(product, '$.{productName}', '{amount}') where id = {id}"
-                cur.execute(addStatement)
+                add_statement = f"update vendingmachine set product = json_insert(product, '$.{product_name}', '{amount}') where id = {id}"
+                cur.execute(add_statement)
         else:
             return "Dont have this machine id"
-        print(productName)
+        print(product_name)
         print(amount)
         mysql.connection.commit()
         cur.close()
@@ -91,30 +97,31 @@ def addProduct():
     else:
         return None
 
+
 @app.route("/edit-product", methods=['POST'])
-def editProduct():
+def edit_product():
     if request.method == 'POST':
         cur = mysql.connection.cursor()
         id = request.args.get("machineid")
-        productName = request.args.get("name")
+        product_name = request.args.get("name")
         amount = request.args.get("amount")
-        if productName == None or amount == None or id == None:
+        if product_name == None or amount == None or id == None:
             return "Plese put machineid, product and amount"
-        checkIdStatement = f"Select id from vendingmachine where id = {id}"
-        result_value = cur.execute(checkIdStatement)
-        #check whether there is this machine id or not 
+        check_id_statement = f"Select id from vendingmachine where id = {id}"
+        result_value = cur.execute(check_id_statement)
+        # check whether there is this machine id or not
         if result_value > 0:
-            checkProductStatement = f"Select json_extract(product, '$.{productName}') from vendingmachine where id = {id} and JSON_EXTRACT(product, '$.{productName}') IS NOT NULL"
-            productresult = cur.execute(checkProductStatement)
-            #checkwhether there is this product or not
-            if productresult > 0:
-                addStatement = f"update vendingmachine set product = json_replace(product, '$.{productName}', '{amount}') where id = {id}"
-                cur.execute(addStatement)
+            check_product_statement = f"Select json_extract(product, '$.{product_name}') from vendingmachine where id = {id} and JSON_EXTRACT(product, '$.{product_name}') IS NOT NULL"
+            product_result = cur.execute(check_product_statement)
+            # check whether there is this product or not
+            if product_result > 0:
+                add_statement = f"update vendingmachine set product = json_replace(product, '$.{product_name}', '{amount}') where id = {id}"
+                cur.execute(add_statement)
             else:
                 return "There is no this product in this machine"
         else:
             return "Dont have this machine id"
-        print(productName)
+        print(product_name)
         print(amount)
         mysql.connection.commit()
         cur.close()
@@ -122,34 +129,36 @@ def editProduct():
     else:
         return None
 
+
 @app.route("/remove-product", methods=['POST'])
-def removeProduct():
+def remove_product():
     if request.method == 'POST':
         cur = mysql.connection.cursor()
         id = request.args.get("machineid")
-        productName = request.args.get("name")
-        if productName == None or id == None:
+        product_name = request.args.get("name")
+        if product_name is None or id is None:
             return "Plese put both machineid and product"
-        checkIdStatement = f"Select id from vendingmachine where id = {id}"
-        result_value = cur.execute(checkIdStatement)
-        #check whether there is this machine id or not 
+        check_id_statement = f"Select id from vendingmachine where id = {id}"
+        result_value = cur.execute(check_id_statement)
+        # check whether there is this machine id or not
         if result_value > 0:
-            checkProductStatement = f"Select json_extract(product, '$.{productName}') from vendingmachine where id = {id} and JSON_EXTRACT(product, '$.{productName}') IS NOT NULL"
-            productresult = cur.execute(checkProductStatement)
-            #checkwhether there is this product or not
-            if productresult > 0:
-                addStatement = f"update vendingmachine set product = json_remove(product, '$.{productName}') where id = {id}"
-                cur.execute(addStatement)
+            check_product_statement = f"Select json_extract(product, '$.{product_name}') from vendingmachine where id = {id} and JSON_EXTRACT(product, '$.{product_name}') IS NOT NULL"
+            product_result = cur.execute(check_product_statement)
+            # check whether there is this product or not
+            if product_result > 0:
+                add_statement = f"update vendingmachine set product = json_remove(product, '$.{product_name}') where id = {id}"
+                cur.execute(add_statement)
             else:
                 return "There is no this product in this machine"
         else:
             return "Dont have this machine id"
-        print(productName)
+        print(product_name)
         mysql.connection.commit()
         cur.close()
         return "Sucessfully Removed"
     else:
         return None
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- create_table.sql
+++ create_table.sql
@@ -3,6 +3,19 @@ create table softeng.vendingmachine
     id           int auto_increment
         primary key,
     machine_name varchar(255) null,
-    location     varchar(255) null,
-    product      json         null
+    location     varchar(255) null
 );
+
+create table softeng.product
+(
+
+    product_id           int auto_increment
+        primary key,
+        constraint product___fk
+        foreign key (machine_id) references vendingmachine (id)
+            on update cascade on delete cascade,
+    machine_id           int,
+    product_name         varchar(255) null,
+    amount               int default 0
+);
+
